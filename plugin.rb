35b6f80f9f5b179a69589ea748e4c9efbcbce587
Anders Elmgren <anders.e.us@gmail.com>
2016-07-06 10:20:00 -0500

    initial plugin test commit

---
+++ plugin.rb
@@ -0,0 +1,6 @@
+# name: discourse-social-feed
+# about: plugin that will display a post freed from a social network
+# version: 0.1
+# authors: Anders Elmgren
+
+enabled_site_setting :awesomeness_enabled
